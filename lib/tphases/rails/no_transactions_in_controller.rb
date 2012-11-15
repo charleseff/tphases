6a6da844b689c70c28bc96926f6cf20efdb7f3e5
Charles Finkel <tech_ops+charles@change.org>
2012-11-15 15:35:27 -0800

    surround whole action with no transactions block

--- lib/tphases/rails/no_transactions_in_controller.rb
+++ lib/tphases/rails/no_transactions_in_controller.rb
@@ -7,10 +7,17 @@ module TPhases
         def ensure_no_transactions_on(*actions)
           actions_array = *actions
           class_variable_set(:@@no_transaction_actions, actions_array.flatten)
+          around_filter :no_transactions_around_filter, :only => actions_array
         end
 
+      end
+
+      private
+      def no_transactions_around_filter
+        TPhases.no_transactions_phase { yield }
       end
     end
 
   end
+
 end
