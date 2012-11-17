00c33be77f347301ad2d31747fdf4b6af475623a
Charles Finkel <tech_ops+charles@change.org>
2012-11-17 02:28:05 -0800

    removed test script

--- test_script.rb
+++
@@ -1,5 +0,0 @@
-require 'pry'
-require 'active_record'
-dbconfig = YAML::load(File.open('spec/fixtures/database.yml'))
-ActiveRecord::Base.establish_connection(dbconfig)
-puts ActiveRecord::Base.connection.select_all('select * from posts')
